dea3bb9f5258f721900815d563b17aa2db52e283
BeamPA <panitabeem@gmail.com>
2025-01-30 16:31:09 +0700

    th

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import Sidebar from './Sidebar/Sidebar';
-import Login from './Page_Login/login';
-import Install from './Page_Install/main-install';
-import SNPPmain from './Page_SNPPmain/SNPPmain';
+import { Route, Routes, useLocation } from "react-router-dom";
+import Sidebar from "./Sidebar/Sidebar";
+import Login from "./Page_Login/login";
+import Install from "./Page_Install/main-install";
+import SNPPmain from "./Page_SNPPmain/SNPPmain";
+import ForgotPassword from "./Page_Login/forgot";
 
-function Layout() {
+function App() {
   const location = useLocation();
   const showSidebar = location.pathname !== "/"; // ถ้าอยู่หน้า Login ไม่แสดง Sidebar
 
   return (
-    <div style={{ display: 'flex' }}>
-      {showSidebar && <Sidebar />} {/* แสดง Sidebar เฉพาะเมื่อไม่ใช่หน้า Login */}
-      <div style={{ flex: 1, padding: '20px' }}>
+    <div style={{ display: "flex" }}>
+      {showSidebar && <Sidebar />}{" "}
+      {/* แสดง Sidebar เฉพาะเมื่อไม่ใช่หน้า Login */}
+      <div style={{ flex: 1, padding: "20px" }}>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
           <Route path="/snpp" element={<SNPPmain />} />
           <Route path="/install" element={<Install />} />
         </Routes>
       </div>
     </div>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <Layout />
-    </Router>
   );
 }
 
